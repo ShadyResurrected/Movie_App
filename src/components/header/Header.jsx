2319dc328a27155b03b5b7d0e838a27f6d43a8c5
ShadyResurrected <cchandra554@gmail.com>
2023-04-03 01:13:09 +0530

    Completed Header,Footer and ongoing Carousel
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
-import './style.scss'
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export default Header
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
+const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation(); // this hook will store the location of current webpage 
+
+  // whenever the location of page is changed, we will scroll to the top
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [location])
+
+  // transition in navbar 
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else setShow("show");
+
+    }else setShow("top")
+
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+    setMobileMenu(false);
+  };
+
+  const searchQueryHandler = (e) => {
+    // only execute this method when the enter key is pressed and the length of typed text is not null
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      // to close the search bar after 1 second
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            Tv Shows
+          </li>
+          <li className="menuItem">
+            <HiOutlineSearch onClick={openSearch} />
+          </li>
+        </ul>
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {/* display hamburger icon or close icon */}
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for movies or tv shows.."
+                onKeyUp={searchQueryHandler}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
