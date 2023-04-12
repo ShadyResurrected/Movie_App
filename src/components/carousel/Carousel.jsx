29e5278bbd6612182f869c68119ddd515b3e179b
ShadyResurrected <cchandra554@gmail.com>
2023-04-12 11:44:05 +0530

    Completed details section
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -16,7 +16,7 @@ import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 
-const Carousel = ({ data, loading, endpoint }) => {
+const Carousel = ({ data, loading, endpoint,title }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
@@ -50,6 +50,7 @@ const Carousel = ({ data, loading, endpoint }) => {
   return (
     <div className="carousel">
       <ContentWrapper>
+        {title && <div className="carouselTitle">{title}</div>}
         <BsFillArrowLeftCircleFill
           className="carouselLeftNav arrow"
           onClick={() => navigation("left")}
